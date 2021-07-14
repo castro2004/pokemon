5ac931f2d887588fee572e89ae6bae7d8fa30ad4
Juan David Castro <jc300159@gmail.com>
2021-07-14 12:48:22 -0600

    Inicializar todo de nuevo

--- Batalla.java
+++ Batalla.java
@@ -1,88 +1,10 @@
-import java.util.scanner;
-public class Batalla {
+  import java.util.Scanner;
 
-  public static void main(String[] args){
-      Sistem .out.println("Nombre jugador 1");
-      String nombre = leer.nextLine();
-      int seleccionar_opcion = 0;
-      int numero1 = 0;
-      int numero2 = 0;
-      int resultado = 0;
+  public class Batalla {
 
-      do{
+    private static Scanner teclado = new Scanner(System.in);
 
-        System.out.println("\n");
-        System.out.println("Seleccionar opcion: ");
-        System.out.println("Opcion 1");
-        System.out.println("Opcion 2");
-        System.out.println("Opcion 3");
-        seleccionar_opcion = leer.nextInt();
-
-        if(seleccionar_opcion !=5){
-          System.out.println("ingrese usuario 1: ");
-          usuario1 = leer.nextInt();
-        
-        
-        }
-
-        switch (seleccionar_opcion) {
-          case 1:
-                  seleccionar_opcion = 1;
-                  resultado = numero1;
-                  System.out.println("sus pokemones son" + pikachu + Luxray + Jolteon);
-            break;
-        
-          default:
-            break;
-          }
-        }
-
-      }
-      System.out.println("Bienvenidos a la batalla pokemon!")
-      Pokemon pikachu = new Pokemon("pikachu", 4);
-      Pokemon Luxray = new Pokemon("Luxray", 4);
-      Pokemon Jolteon = new Pokemon("Jolteon ", 4);
-      Pokemon charmander = new Pokemon("Charmander", 2);
-      Pokemon Vulpix = new Pokemon("Vulpix", 2);
-      Pokemon squirtle = new Pokemon("Squirtle", 1);
-      Pokemon Totodile = new Pokemon("Totodile", 1);
-      Pokemon Pansage = new Pokemon("Pansage", 3);
-      Pokemon Pidgeot = new Pokemon("Pidgeot", 3);
-      Pokemon Snorlax = new Pokemon("Snorlax", 3);
-
-        System.out.println("Los combatientes el dia de hoy son: ");
-        System.out.println(pikachu.toString());
-        System.out.println(Luxray.toString());
-        System.out.println(Jolteon.toString());
-        System.out.println(charmander.toString());
-        System.out.println(Vulpix.toString());
-        System.out.println(squirtle.toString());
-        System.out.println(Totodile.toString());
-        System.out.println(Pansage.toString());
-        System.out.println(Pidgeot.toString());
-        System.out.println(Snorlax.toString());        
-
-        int ataquePikachu = pikachu.getAtaque();
-        int ataqueLuxray = Luxray.getAtaque();
-        int ataqueJolteon = Jolteon.getAtaque();
-        int ataqueCharmander = charmander.getAtaque();
-        int ataqueVulpix = Vulpix.getAtaque();
-        int ataqueSquirtle = squirtle.getAtaque();
-        int ataqueTotodile = Totodile.getAtaque();
-        int ataquePansage = Pansage.getAtaque();
-        int ataquePidgeot = Pidgeot.getAtaque();
-        int ataqueSnorlax = Snorlax.getAtaque();
-
-        System.out.println("Ataca " + squirtle.nombre + " con un ataque de " + ataqueSquirtle);
-        charmander.golpe(squirtle);
-        System.out.println(charmander.toString());
-
-        System.out.println("Ataca " + charmander.nombre + " con un ataque de " + ataqueCharmander);
-        squirtle.golpe(charmander);
-        System.out.println(squirtle.toString());
-
+    public static void main(String[] args){
+      
     }
   }
-    
-  
-}
--- Tipo.java
+++ Tipo.java
@@ -1,48 +1,4 @@
 public class Tipo{
-    int tipo; //1=Agua, 2=Fuego, 3=Planta, 4=Electrico, 5=Volador
-    int debilidad; 
-    int debilidad2; 
-
-    public Tipo(int tipo){
-        this.tipo = tipo;
-        if(tipo==1){
-            debilidad = 2;
-            debilidad2 = 0;
-        } else if (tipo == 2){
-            debilidad = 3;
-            debilidad2 = 0;
-        } else if (tipo == 3){
-            debilidad = 1;
-            debilidad2 = 0;
-        } else if (tipo == 4){
-            debilidad = 1;
-            debilidad2 = 5;
-        } else if (tipo == 5){
-            debilidad = 3;
-            debilidad2 = 0;
-        }
-    }
-
-    public int getTipo(){
-        return tipo;
-    }
-
-    public String getTipoString(){
-        String tipoPokemon="";
-        if(tipo == 1){
-            tipoPokemon = "Agua";
-        } else if (tipo == 2){
-            tipoPokemon = "Fuego";
-        } else if (tipo == 3){
-            tipoPokemon = "Planta";
-        } else if (tipo == 4){
-            tipoPokemon = "Electrico";
-        } else if (tipo == 5){
-            tipoPokemon = "Volador";
-        }
-        return tipoPokemon;
-    }
-
-
+    
 
 }
